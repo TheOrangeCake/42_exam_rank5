79b3712f72c4831201a3235c563aa0bcd6a912d8
TheOrangeCake <vietfox94@gmail.com>
2025-11-20 14:45:35 +0100

    ok

--- lvl_01/01_solution/life/practice.c
+++ lvl_01/01_solution/life/practice.c
@@ -1,125 +1,96 @@
-#include <stdlib.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   practice.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/20 12:34:36 by hoannguy          #+#    #+#             */
+/*   Updated: 2025/11/20 12:34:36 by hoannguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-
-void free_map(char **map, int height) {
-    if (map == NULL)
-        return;
-    for (int y = 0; y < height; y++) {
-        free(map[y]);
-    }
-    free(map);
-}
-
-char **init_map(int width, int height) {
-    char **map = malloc(sizeof(char*) * height);
-    if (map == NULL)
-        return NULL;
-    for (int y = 0; y < height; y++) {
-        map[y] = malloc(sizeof(char) * width);
-        if (map[y] == NULL) {
-            free_map(map, height);
-            return NULL;
-      }
-    }
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            map[y][x] = ' ';
-        }
-    }
-    return map;
-}
-
-void print_map(char **map, int height, int width) {
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            putchar(map[y][x]);
-        }
-        putchar('\n');
-    }
-}
-
-int neigbors(char **map, int height, int width, int y, int x) {
-    int count = 0;
-    for (int yi = y - 1; yi <= y + 1; yi++) {
-        for (int xi = x - 1; xi <= x + 1; xi++) {
-            if (yi == y && xi == x)
-                continue;
-            if (yi >= 0 && xi >= 0 && yi < height && xi < width) {
-                if (map[yi][xi] == 'O')
-                    count++;
-            }
-        }
-    }
-    return count;
-}
-
-void iter_map(char **map, int height, int width) {
-    char **temp = init_map(width, height);
-    if (temp == NULL)
-        return;
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            int count = neigbors(map, height, width, y, x);
-            if (map[y][x] == 'O') {
-                if (count == 2 || count == 3)
-                    temp[y][x] = 'O';
-            } else {
-                if (count == 3)
-                    temp[y][x] = 'O';
-            }
-        }
-    }
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++)
-            map[y][x] = temp[y][x];
-    }
-    free_map(temp, height);
-}
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(int ac, char **av) {
-    if (ac != 4)
-        return -1;
+    if (ac != 4) {
+        putchar('e');
+        return 1;
+    }
+
     int width = atoi(av[1]);
     int height = atoi(av[2]);
     int iter = atoi(av[3]);
-    if (width <= 0 || height <= 0 || iter < 0)
-        return -1;
-    char **map = init_map(width, height);
-    if (map == NULL)
-        return -1;
-    int penX = 0;
-    int penY = 0;
-    int penDown = 0;
-    char command;
-    while (read(0, &command, 1) > 0) {
-        if (penDown)
-            map[penY][penX] = 'O';
-        switch(command) {
-            case 'w':
-                if (penY > 0)
-                    penY--;
-                continue;
-            case 's':
-                if (penY < height - 1)
-                    penY++;
-                continue;
-            case 'a':
-                if (penX > 0)
-                    penX--;
-                continue;
-            case 'd':
-                if (penX < width - 1)
-                    penX++;
-                continue;
-            case 'x':
-                penDown = !penDown;
-                continue;
+    int y = 1;
+    int x = 1;
+    int pen = 0;
+    int tab[2][height + 2][width + 2];
+
+    for (int i = 0; i < 2; i++) {
+        for (int y = 0; y < height + 2; y++) {
+            for (int x = 0; x < width + 2; x++) {
+                tab[i][y][x] = 0;
+            }
         }
     }
-    for (int i = 0; i < iter; i++)
-        iter_map(map, height, width);
-    print_map(map, height, width);
-    free_map(map, height);
+    char cmd;
+    while (read(0, &cmd, 1) > 0) {
+        if (cmd == 'w' && y > 1)
+            y--;
+        else if (cmd == 's' && y < height)
+            y++;
+        else if (cmd == 'a' && x > 1)
+            x--;
+        else if (cmd == 'd' && x < width)
+            x++;
+        else if (cmd == 'x')
+            pen = !pen;
+
+        if (pen) {
+            tab[0][y][x] = 1;
+        }
+    }
+
+    for (int i = 0; i < iter; i++) {
+        for (int h = 0; h < height + 2; h++) {
+            for (int w = 0; w < width + 2; w++) {
+                tab[(i + 1) % 2][h][w] = 0;
+            }
+        }
+        for (int h = 1; h < height + 1; h++) {
+            for (int w = 1; w < width + 1; w++) {
+                int nb = 0;
+                for (int y = -1; y < 2; y++) {
+                    for (int x = -1; x < 2; x++) {
+                        if (x == 0 && y == 0)
+                            continue;
+                        nb += tab[i % 2][y + h][x + w];
+                    }
+                    if (tab[i % 2][h][w] == 1) {
+                        if (nb == 2 || nb == 3)
+                            tab[(i + 1) % 2][h][w] = 1;
+                        else
+                            tab[(i + 1) % 2][h][w] = 0;
+                    } else {
+                        if (nb == 3)
+                            tab[(i + 1) % 2][h][w] = 1;
+                        else
+                            tab[(i + 1) % 2][h][w] = 0;
+                    }
+                }
+            }
+        }
+    }
+    for (int h = 1; h < height + 1; h++) {
+        for (int w = 1; w < width + 1; w++) {
+            if (tab[iter % 2][h][w] == 1)
+                putchar('O');
+            else
+                putchar(' ');
+        }
+        putchar('\n');
+    }
     return 0;
 }
--- lvl_01/01_solution/life/solution.c
+++ lvl_01/01_solution/life/solution.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/18 13:47:05 by jquinodo          #+#    #+#             */
-/*   Updated: 2025/11/19 12:31:00 by hoannguy         ###   ########.fr       */
+/*   Created: 2025/11/20 09:52:25 by hoannguy          #+#    #+#             */
+/*   Updated: 2025/11/20 12:47:49 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,67 +14,92 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int ac , char **av)
-{
-	if(ac != 4)
-		return(putchar('e'), 1);
+int main(int ac, char **av) {
+    int width = 0;
+    int height = 0;
+    int iter = 0;
+    int pen = 0;
+    int x = 1;
+    int y = 1;
 
-	int width = atoi(av[1]);
-	int heigheight = atoi(av[2]);
-	int iter = atoi(av[3]);
-	int pen = 0;
-	int x = 1;
-	int y = 1;
-	int tab[2][width+2][height+2];
+    if (ac != 4) {
+        putchar('e');
+        return 1;
+    }
+    width = atoi(av[1]);
+    height = atoi(av[2]);
+    iter = atoi(av[3]);
+    int tab[2][height + 2][width + 2];
 
-	for(int i = 0 ; i < 2 ; i++)
-		for(int h = 0; h < height + 2 ; h++)
-			for(int l = 0; l < width + 2 ; l++)
-				tab[i][l][h] = 0;
-			
-	char cmd;
-	while(read(0, &cmd, 1) > 0)
-	{
-		if 		(cmd == 'w' && y > 1 )	y--;
-		else if (cmd == 'a' && x > 1)	x--;
-		else if (cmd == 's' && y < height)	y++;
-		else if (cmd == 'd' && x < width)	x++;
-		else if (cmd == 'x' )	 pen = !pen;
+    // initiate the map
+    for (int i = 0; i < 2; i++) {
+        for (int h = 0; h < height + 2; h++)
+            for (int w = 0; w < width + 2; w++)
+                tab[i][h][w] = 0;
+    }
 
-		if(pen)
-			tab[0][x][y] = 1;
-	}
+    // get the command
+    char cmd;
+    while (read(0, &cmd, 1) > 0) {
+        if (cmd == 'w' && y > 1)
+            y--;
+        else if (cmd == 's' && y < height)
+            y++;
+        else if (cmd == 'a' && x > 1)
+            x--;
+        else if (cmd == 'd' && x < width)
+            x++;
+        else if (cmd == 'x')
+            pen = !pen;
+        if (pen)
+            tab[0][y][x] = 1;
+    }
 
-	for(int i = 0 ; i < iter; i++){
-		for(int h = 1; h <= height; h++){
-			for(int l = 1; l <= width; l++){
-				 	int nb = 0;
-         
-				for(int dy = -1; dy < 2; dy++){
-					for(int dx = -1; dx < 2 ; dx++){
-						if(dx || dy)
-							nb += tab[i % 2][dx + l][dy + h];
-					}
-				}
+    // iter
+    for (int i = 0; i < iter; i++) {
+        // reset 
+        for (int h = 0; h < height + 2; h++) {
+            for (int w = 0; w < width + 2; w++) {
+                tab[(i + 1) % 2][h][w] = 0;
+            }
+        }
 
-				if(tab[i % 2][l][h] == 1)
-					if(nb == 2 || nb == 3)
-						tab[(i+1)%2][l][h] = 1;
-					else
-						tab[(i+1)%2][l][h] = 0;
-				else
-					if (nb == 3)
-						tab[(i+1)%2][l][h] = 1;
-					else
-						tab[(i+1)%2][l][h] = 0;
-			}
-		}
-	}
-	for(int h = 1; h <= height; h++){
-		for(int l = 1; l <= width; l++){
-			putchar(tab[iter % 2][l][h] ? 'O': ' ');
-			}
-			putchar('\n');
-			}
-	return 0;
+        for (int h = 1; h <= height; h++) {
+            for (int w = 1; w <= width; w++) {
+                int nb = 0;
+                // calculate neighbor
+                for (int y = -1; y < 2; y++) {
+                    for (int x = -1; x < 2; x++) {
+                        if (x == 0 && y == 0)
+                            continue;
+                        nb += tab[i % 2][y + h][x + w];
+                    }
+                }
+                // populate
+                if (tab[i % 2][h][w] == 1) {
+                    if (nb == 2 || nb == 3)
+                        tab[(i + 1) % 2][h][w] = 1;
+                    else
+                        tab[(i + 1) % 2][h][w] = 0;
+                } else {
+                    if (nb == 3)
+                        tab[(i + 1) % 2][h][w] = 1;
+                    else
+                        tab[(i + 1) % 2][h][w] = 0;
+                }
+            }
+        }
+    }
+
+    // print
+    for (int h = 1; h <= height; h++) {
+        for (int w = 1; w <= width; w++) {
+            if (tab[iter % 2][h][w] == 0)
+                putchar(' ');
+            else
+                putchar('O');
+        }
+        putchar('\n');
+    }
+    return 0;
 }
